cc686e36a86c2c49490b3affc5fa92ac55c62317
Scotow <contact@scotow.com>
2020-03-27 23:12:39 +0100

    First version (dirty)

---
+++ src/bin/main.rs
@@ -0,0 +1,58 @@
+use rustforge::talisman::*;
+
+use clap::clap_app;
+use rayon::prelude::*;
+
+fn main() {
+    let matches = clap_app!(myapp =>
+        (version: "1.0")
+        (about: "Calculate best talismans reforge")
+        (@arg COMMON: -c --common +takes_value "Number of common talisman(s)")
+        (@arg UNCOMMON: -u --uncommon +takes_value "Number of uncommon talisman(s)")
+        (@arg RARE: -r --rare +takes_value "Number of rare talisman(s)")
+        (@arg EPIC: -e --epic +takes_value "Number of epic talisman(s)")
+        (@arg LEGENDARY: -l --legendary +takes_value "Number of legendary talisman(s)")
+    ).get_matches();
+
+    let mut talismans = Vec::new();
+    for _ in 0..matches.value_of("COMMON").map(|s| s.parse::<u32>().unwrap()).unwrap_or(0) {
+        talismans.push(Talisman{
+            rarity: Rarity::Common
+        })
+    }
+    for _ in 0..matches.value_of("UNCOMMON").map(|s| s.parse::<u32>().unwrap()).unwrap_or(0) {
+        talismans.push(Talisman{
+            rarity: Rarity::Uncommon
+        })
+    }
+    for _ in 0..matches.value_of("RARE").map(|s| s.parse::<u32>().unwrap()).unwrap_or(0) {
+        talismans.push(Talisman{
+            rarity: Rarity::Rare
+        })
+    }
+    for _ in 0..matches.value_of("EPIC").map(|s| s.parse::<u32>().unwrap()).unwrap_or(0) {
+        talismans.push(Talisman{
+            rarity: Rarity::Epic
+        })
+    }
+    for _ in 0..matches.value_of("LEGENDARY").map(|s| s.parse::<u32>().unwrap()).unwrap_or(0) {
+        talismans.push(Talisman{
+            rarity: Rarity::Legendary
+        })
+    }
+
+    //let base_crit_chance = matches.value_of("BASE_CRIT_CHANCE").map(|s| s.parse::<u64>().unwrap()).unwrap_or(0);
+    let base_crit_chance = 62;
+
+    let end = 2_u64.pow(talismans.len() as u32) - 1;
+    let chunk = end / (num_cpus::get() as u64);
+
+    (0..(num_cpus::get() as u64)).into_par_iter().for_each(|i| {
+        let best_config = solve(&talismans, i * chunk, (i + 1) * chunk, base_crit_chance);
+
+        let score = score(&talismans, best_config);
+        let current = evaluate(score, base_crit_chance);
+        println!("Score: {}, config: {:028b}", current, best_config);
+    });
+    //println!("{:#064b}", solve(&talismans, 0, end, base_crit_chance));
+}
---
+++ src/talisman.rs
@@ -0,0 +1,100 @@
+#[derive(Copy, Clone, Debug)]
+#[repr(u8)]
+pub enum Rarity {
+    Common = 0,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary,
+}
+
+#[derive(Debug)]
+pub enum Reforge {
+    CriticalChance = 0,
+    CriticalDamage
+}
+
+#[derive(Debug)]
+pub struct Stats {
+    pub critical_chance: u64,
+    pub critical_damage: u64
+}
+
+const STATS_MAPPING: [[Stats; 2]; 5] = [
+    [
+        Stats{critical_chance: 1, critical_damage: 1},
+        Stats{critical_chance: 0, critical_damage: 3},
+    ],
+    [
+        Stats{critical_chance: 2, critical_damage: 2},
+        Stats{critical_chance: 0, critical_damage: 5},
+    ],
+    [
+        Stats{critical_chance: 3, critical_damage: 2},
+        Stats{critical_chance: 0, critical_damage: 8},
+    ],
+    [
+        Stats{critical_chance: 6, critical_damage: 3},
+        Stats{critical_chance: 0, critical_damage: 12},
+    ],
+    [
+        Stats{critical_chance: 8, critical_damage: 5},
+        Stats{critical_chance: 0, critical_damage: 15},
+    ],
+];
+
+#[derive(Debug)]
+pub struct Talisman {
+    pub rarity: Rarity
+}
+
+pub fn solve(talismans: &[Talisman], start: u64, end: u64, base_crit_chance: u64) -> u64 {
+    let mut max = 0;
+    let mut res = 0;
+    for i in start..end {
+        let score = score(talismans, i);
+        let current = evaluate(score, base_crit_chance);
+        if current > max {
+            max = current;
+            res = i;
+        }
+    }
+
+    res
+}
+
+pub fn score(talismans: &[Talisman], mut config: u64) -> Stats {
+    let mut total_stats = Stats{ critical_chance: 0, critical_damage: 0 };
+
+    let mut talismans = talismans.iter();
+    while config != 0 {
+        let current = talismans.next().expect("should have find one here");
+        let choice = config & 1;
+
+        let stats = &STATS_MAPPING[current.rarity as u8 as usize][choice as usize];
+        total_stats.critical_chance += stats.critical_chance;
+        total_stats.critical_damage += stats.critical_damage;
+
+        config = config >> 1;
+    }
+
+    for talisman in talismans {
+        let stats = &STATS_MAPPING[talisman.rarity as u8 as usize][0];
+        total_stats.critical_chance += stats.critical_chance;
+        total_stats.critical_damage += stats.critical_damage;
+    }
+
+    total_stats
+}
+
+pub fn evaluate(stats: Stats, base_crit_chance: u64) -> u64 {
+    let mut res: u64 = 0;
+    res += stats.critical_damage;
+    if base_crit_chance + stats.critical_chance > 100 {
+        res += 100_000
+    } else {
+        res += (base_crit_chance + stats.critical_chance) * 1_000
+    }
+
+    res
+}
